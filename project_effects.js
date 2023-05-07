d7c05b4f9c932c50a420865f223f15ced8bb79b3
Lewis Patterson <lewispatterson90@gmail.com>
2023-05-07 22:10:03 +0100

    Added initial styling to accordion

--- project_effects.js
+++ project_effects.js
@@ -26,3 +26,28 @@ document.querySelector("h1").onmouseover = event => {
     iteration += 1 / 3;
   }, 30);
 }
+
+
+const accordion = document.querySelector(".accordion");
+
+accordion.addEventListener('click', (e) => {
+  const activePanel = e.target.closest(".accordion-panel");
+  if (!activePanel) return;
+  toggleAccordion(activePanel);
+})
+
+function toggleAccordion(activatingPanel){
+  const buttons = activatingPanel.parentElement.querySelectorAll("button");
+  const contents = activatingPanel.parentElement.querySelectorAll(".accordion-content");
+
+  buttons.forEach((button) =>{
+    button.setAttribute("aria-expanded", false);
+  })
+
+  contents.forEach((content) =>{
+    content.setAttribute("aria-hidden", true);
+  })
+
+  activatingPanel.querySelector("button").setAttribute("aria-expanded", true);
+  activatingPanel.querySelector(".accordion-content").setAttribute("aria-hidden", false);
+}
